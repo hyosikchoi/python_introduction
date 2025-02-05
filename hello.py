5a59306c228611e65cadaef15c5d8844f1803e96
hyosikchoi <huchoi24@naver.com>
2025-02-06 00:41:18 +0900

    Add - hello world of python

---
+++ hello.py
@@ -0,0 +1,16 @@
+import sys
+
+print("Hello, World!")
+print(sys.version) # 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)]
+
+if 5 > 2:
+    print("Five is greater than two!")
+    print("Five is greater than two!")
+
+x = 5
+x = "Sally"
+y = "Hello, World!"
+
+print(x, y)
+
+
