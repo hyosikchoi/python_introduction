ae5499757d96f3dcb97d79fdac7cd9cedaeb811e
hyosikchoi <huchoi24@naver.com>
2025-02-06 00:55:51 +0900

    Add - Python variables

--- hello.py
+++ hello.py
@@ -7,10 +7,39 @@ if 5 > 2:
     print("Five is greater than two!")
     print("Five is greater than two!")
 
-x = 5
-x = "Sally"
+x = 5 # x type int
+x = "Sally" # x is now of type str
 y = "Hello, World!"
 
 print(x, y)
 
 
+a = str(3)
+b = int(3)
+z = float(3)
+
+print(a,b,z) # '3', 3 , 3.0
+
+# Multiple Variables
+
+x, y, z = "Apple", "Banana" , "Orange"
+print(x)
+print(y)
+print(z)
+
+# Unpack a Collection
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "awesome"
+
+def myfunc():
+    x = "fantastic"
+    print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
