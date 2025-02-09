0400b7d454afd15648c0f885a5db3a7caac11643
hyosikchoi <huchoi24@naver.com>
2025-02-09 19:39:42 +0900

    Add - List.py

---
+++ list.py
@@ -0,0 +1,15 @@
+a = ["apple", "banana" , "cherry"]
+print(a[0]) # apple
+
+print(a[-1]) # cherry
+
+
+# Change Range items
+a[0:2] = ["orange", "watermelon"]
+
+print(a)
+
+
+# 
+a.insert(4,"peach")
+print(a)
